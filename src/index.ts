eb08e3e7a88921b24b414dc6b1d08b8cf0880d31
Reselim <notreselim@gmail.com>
2021-09-28 20:14:41 -0400

    Change initial function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import Bot from "./Bot"
 import logger from "./util/logger"
 
-async function init(): Promise<Bot> {
+(async () => {
 	const bot: Bot = new Bot({
 		clientId: process.env.CLIENT_ID as string,
 		guildId: process.env.GUILD_ID as string,
 	})
-	await bot.login(process.env.TOKEN as string)
-	return bot
-}
 
-init().then((bot) => {
+	await bot.login(process.env.TOKEN as string)
+
 	logger.info(`Logged in as ${bot.client.user?.username}`)
-}, (error) => {
-	throw error
-})
+})()
