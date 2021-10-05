91636b3ffa6051f8e504f7b8ecf4172d91852f30
Reselim <notreselim@gmail.com>
2021-10-04 22:27:08 -0400

    Add colors util

---
+++ src/util/colors.ts
@@ -0,0 +1,8 @@
+import { ColorResolvable } from "discord.js"
+
+export default {
+	brand: "#00897b" as ColorResolvable,
+
+	actionActive: "#00897b" as ColorResolvable,
+	actionInactive: "#5b5b5b" as ColorResolvable,
+}
