db86efd2d9131766b7166f0b4cb009913db1025e
Reselim <notreselim@gmail.com>
2021-10-04 22:32:19 -0400

    Switch to snowflake for config entries

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import root from "app-root-path"
+import { Snowflake } from "discord-api-types"
 
 interface IConfig {
-	channels: { [name: string]: string },
-	roles: { [name: string]: string },
+	channels: { [name: string]: Snowflake },
+	roles: { [name: string]: Snowflake },
 }
 
 const file = process.env.CONFIG_FILE ?? path.resolve(root.path, "config.json")
