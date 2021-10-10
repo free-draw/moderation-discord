779b4367869337fe7744d8beed74cffa748d49e1
Reselim <notreselim@gmail.com>
2021-10-09 22:18:22 -0400

    Update init logging

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import env from "./util/env"
 import log from "./util/log"
 
 (async () => {
+	log.debug("Initializing")
+
 	const bot: Bot = new Bot({
 		token: env.token,
 		clientId: env.clientId,
 		clientSecret: env.clientSecret,
 		guildId: env.guildId,
+		redisUrl: env.redisUrl,
 	})
 
-	log.info("Logging in...")
 	await bot.login()
-	log.info(`Logged in as ${bot.client.user?.username}`)
+
+	log.info(`Connected to Discord: ${bot.client.user?.username}`)
 })()
