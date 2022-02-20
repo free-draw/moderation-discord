3b2000a6b659cdd6a5bd32de81edcb94c705112a
Reselim <notreselim@gmail.com>
2022-02-20 03:38:18 -0500

    Change config stuff to records

--- src/util/option/config.ts
+++ src/util/option/config.ts
@@ -5,8 +5,8 @@ import env from "./env"
 
 type Config = {
 	guild: Snowflake,
-	channels: { [name: string]: Snowflake },
-	roles: { [name: string]: Snowflake },
+	channels: Record<string, Snowflake>,
+	roles: Record<string, Snowflake>,
 }
 
 const file = env.configFile ?? path.resolve(root.path, "config.json")
