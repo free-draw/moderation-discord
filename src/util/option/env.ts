96fdb3fee7e0afeb8e3906e7015d25d40ef3d7e4
Reselim <notreselim@gmail.com>
2024-06-24 23:01:13 -0400

    Revert some changes

--- src/util/option/env.ts
+++ src/util/option/env.ts
@@ -19,6 +19,8 @@ function getEnvironmentVariable(name: string, required?: boolean): string | unde
 const env = {
 	configFile: getEnvironmentVariable("CONFIG_FILE", false),
 	discordToken: getEnvironmentVariable("DISCORD_TOKEN", true),
+	discordClientId: getEnvironmentVariable("DISCORD_CLIENT_ID", true),
+	discordClientSecret: getEnvironmentVariable("DISCORD_CLIENT_SECRET", true),
 	apiUrl: getEnvironmentVariable("API_URL", true),
 	apiToken: getEnvironmentVariable("API_TOKEN", true),
 	redisUrl: getEnvironmentVariable("REDIS_URL", false),
